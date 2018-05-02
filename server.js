e93765bff1f23eddcfdecf5ce029516cde7ea7f0
Dustin Frank <dustintstrange@gmail.com>
2018-05-01 17:31:13 -0700

    beginning of boiler plate

---
+++ server.js
@@ -0,0 +1,24 @@
+//requirements
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const jwt = require("express-jwt");
+const fileUpload = require('express-fileupload');
+
+//middleware
+const bodyParser = require('body-parser');
+
+//express setup
+const app = express();
+const PORT = process.env.PORT || 3001;
+const isDev = process.env.NODE_ENV === 'development';
+
+//fileupload middleware
+app.use(fileUpload())
+
+
+const db = require(path.join(__dirname + '/models'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
